01d5be2b4ff91c5b9ee86301bc1fb63e5d95aed2
Stanley Chibuike <42142189+Stanyke@users.noreply.github.com>
2019-10-21 14:26:36 +0100

    Update script.js
    Update made, added a new div element to get all countries

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // var ddd = document.getElementById('ddd').addEventListener('click', loadREST);
 var details = document.getElementById('details');
 var countryInputted = document.getElementById('country');
+var errorLoad = document.getElementById('errorLoad');
 
 function getCountry()
 {
@@ -29,29 +30,30 @@ function getCountry()
 				
 				<div class='container'>
 					<table class='table table-dark table-hover'>
-					<thead>
-						<tr>
-							<th>Country</th>
-							<th>Region</th>
-							<th>Populations</th>
-							<th>Timezone</th>
-							<th>Currrency</th>
-							<th>Languages</th>
-							<th>Calling Codes</th>
-						</tr>
-					</thead>
-				
-					<tbody>
-						<tr>
-							<td>${dat.name}</td>
-							<td>${dat.region}</td>
-							<td>${dat.population}</td>
-							<td>${dat.timezones}</td>
-							<td>${dat.currencies}</td>
-							<td>${dat.languages}</td>
-							<td>${dat.callingCodes}</td>
-						</tr>
-					</tbody>
+						<thead>
+							<tr>
+								<th>Country</th>
+								<th>Region</th>
+								<th>Populations</th>
+								<th>Timezone</th>
+								<th>Currrency</th>
+								<th>Languages</th>
+								<th>Calling Codes</th>
+							</tr>
+						</thead>
+					
+						<tbody>
+							<tr>
+								<td>${dat.name}</td>
+								<td>${dat.region}</td>
+								<td>${dat.population}</td>
+								<td>${dat.timezones}</td>
+								<td>${dat.currencies}</td>
+								<td>${dat.languages}</td>
+								<td>${dat.callingCodes}</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 
 			`;
@@ -61,7 +63,55 @@ function getCountry()
 		
 		.catch(function(error)
 		{
-			details.innerHTML = error;
+			errorLoad.innerHTML = error;
 		})
 	}
 }
+
+
+
+
+
+
+var gottenCountries = document.getElementById('countriesGotten');
+
+
+getAllToBeClicked = () =>
+{
+	
+	fetch('https://restcountries.eu/rest/v2/all')
+	.then(function(response)
+		{
+			console.log(response);
+			
+			return response.json();
+		})
+		.then(function(data){
+			console.log(data);
+			
+			let html = '';
+			
+			
+			data.forEach(function(dat){
+				html += `
+				
+					<table class='table table-dark table-hover'>
+				
+						<tbody>
+							<tr>
+								<td>${dat.name}</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+
+			`;
+			});
+			gottenCountries.innerHTML = html;
+		})
+		
+		.catch(function(error)
+		{
+			errorLoad.innerHTML = error;
+		})
+	}
